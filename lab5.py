68fb41dc26a033f832e3447411ca4a977f179a5f
pr0stakk <120129343+pr0stakk@users.noreply.github.com>
2023-12-03 16:43:11 +0200

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -10,23 +10,27 @@ class Smartphone:
 class PhoneStore:
     def __init__(self):
         self.inventory = []
+    # This method creates an empty inventory list that will be used to store information about phones
 
     def add_phone(self, smartphone):
         self.inventory.append(smartphone)
+    # This method adds a new smartphone to the store inventory
 
-    def get_phone_price(self,phone):
+    def get_phone_price(self, phone):
         return phone.price
+    # This method takes a phone as an argument and returns its price. It is used to sort phones by price
 
     def find_best_phone(self, max_budget):
         affordable_phones = [
             phone for phone in self.inventory if phone.price <= max_budget]
         sorted_phones = sorted(affordable_phones, key=self.get_phone_price)
-
         return sorted_phones
+    # This method takes a maximum budget as an argument and returns a list of available phones that fall within that budget.
 
 
 phone1 = Smartphone("Model A", 500, ["123-456-7890", "987-654-3210"], 64, 3000)
-phone2 = Smartphone("Model B", 700, ["555-123-4567", "111-222-3333"], 128, 4000)
+phone2 = Smartphone(
+    "Model B", 700, ["555-123-4567", "111-222-3333"], 128, 4000)
 phone3 = Smartphone("Model C", 400, ["999-888-7777", "444-555-6666"], 32, 2500)
 
 store = PhoneStore()
